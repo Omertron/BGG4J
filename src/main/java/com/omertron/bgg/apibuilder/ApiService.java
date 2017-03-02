b4d4334d208e7f32b0d6cde7b2414f75464f71ae
Stuart Boston <omertron@gmail.com>
2017-03-02 15:46:51 +0000

    Remove tabs

--- src/main/java/com/omertron/bgg/apibuilder/ApiService.java
+++ src/main/java/com/omertron/bgg/apibuilder/ApiService.java
@@ -454,10 +454,10 @@ public class ApiService {
      */
     protected static String convertStreamToString(InputStream is) {
         /*
-	     * To convert the InputStream to String we use the BufferedReader.readLine()
-	     * method. We iterate until the BufferedReader return null which means
-	     * there's no more data to read. Each line will appended to a StringBuilder
-	     * and returned as String.
+         * To convert the InputStream to String we use the BufferedReader.readLine()
+         * method. We iterate until the BufferedReader return null which means
+         * there's no more data to read. Each line will appended to a StringBuilder
+         * and returned as String.
          */
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         StringBuilder sb = new StringBuilder();
